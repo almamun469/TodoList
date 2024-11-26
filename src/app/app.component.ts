c03cfc967e274143c1041742d177da03cbd8fa45
almamun469 <almamundewan469@gmail.com>
2024-11-26 18:16:29 +0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Todo-List';
+
+
+  todoList :Todo []=[];
+  todo:Todo = this.initTodo;
+
+
+
+  get initTodo ():Todo{
+    return {
+      Title:'',
+      Id:null
+    }
+  }
+
+  addTodo():void {
+    console.log(this.todo)
+
+
+    if(this.todo.Id){
+
+    this.todoList = this.todoList.map(o=> {
+
+      if(o.Id == this.todo.Id){
+        o.Title = this.todo.Title;
+      }
+      return o;
+    })
+
+
+      // update logic
+    }
+    else {
+      // create logic
+      this.todo.Id = Date.now();
+      this.todoList.push({...this.todo});
+    }
+
+
+
+
+    console.log(this.todoList)
+
+    this.todo =this.initTodo;
+  }
+
+  editTodo(todo:Todo) :void{
+
+    this.todo = {...todo};
+  }
+
+  delteTodo(id:number):void {
+    this.todoList = this.todoList.filter(o=>o.Id!=id);
+
+  }
 }
